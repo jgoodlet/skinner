5195862790851c67f927ff3ba1b2c3592f9f2d5a
Joshua Goodlett <joshuagoodlett@gmail.com>
2012-12-29 15:47:55 -0800

    Tab conversion

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import sys
 
 setup(
-	name="skinner",
-	version="1.0",
-	author="Joshua Goodlett",
-	author_email="joshuagoodlett@gmail.com",
-	url="",
-	download_url="",
-	description=""
+    name="skinner",
+    version="1.0",
+    author="Joshua Goodlett",
+    author_email="joshuagoodlett@gmail.com",
+    url="",
+    download_url="",
+    description=""
 )
