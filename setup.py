75e4212b2df35536a07e467e10bddfa82e3ea32b
Joshua Goodlett <joshuagoodlett@gmail.com>
2013-01-03 22:36:49 -0800

    Fleshing out setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from distutils.core import setup
 import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name="skinner",
     version="1.0",
     author="Joshua Goodlett",
     author_email="joshuagoodlett@gmail.com",
-    url="",
-    download_url="",
-    description=""
+    url="https://github.com/jgoodlet/skinner",
+    description="A Python static site generator.",
+    install_requires="",
+    entry_points={
+        "console_scripts": ["skinner = skinner.cli.main"]
+    }
 )
